234baca252dd85f0d8151b2d40c200cd119938c9
MAGICPEA <MAGICPEA@UW.EDU>
2023-01-27 15:03:02 -0800

    new tiny csv article file and the r scipt to recreate it

---
+++ sample.R
@@ -0,0 +1,3 @@
+short_data <- read.csv("/Users/marieoconnell/Desktop/Infonauts/output_article.csv", nrows=100, header=TRUE, sep=";")
+View(short_data)
+write.csv(short_data,'/Users/marieoconnell/Desktop/Infonauts/')
